d6f7494edc7fb018194a7cd2300b3372bb40a4de
Steven Aiello <steven.aiello@agoge.io>
2023-06-21 21:29:45 -0400

    update certbot config

--- set_certbot.sh
+++ set_certbot.sh
@@ -2,8 +2,11 @@
 
 domain=$1
 www=www.$domain
+email=admin@$domain
 
 echo "Getting certificates for "$domain" and "$www
 echo $domain
 echo $www
-certbot run --nginx -d $domain -d $www
+echo $email
+
+certbot run --nginx -n -m $email -d $domain -d $www
